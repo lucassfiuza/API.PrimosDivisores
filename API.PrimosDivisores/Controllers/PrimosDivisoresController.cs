e5e97f3f062c8db7df871980ca25dab45b988f23
Lucas Fiuza <Lucas Fiuza@DESKTOP-JR6UUKB>
2021-12-15 12:40:02 -0300

    Adicionar arquivos de projeto.

---
+++ API.PrimosDivisores/Controllers/PrimosDivisoresController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.PrimosDivisores.Estruturas;
+
+namespace API.PrimosDivisores.Controllers
+{
+    [ApiController]
+    [Route("PrimosDivisores")]
+    public class PrimosDivisoresController : ControllerBase
+    {
+
+        private readonly ILogger<PrimosDivisoresController> _logger;
+
+        public PrimosDivisoresController(ILogger<PrimosDivisoresController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(int numeroEntrada)
+        {
+            List<int> aux = new List<int>();
+            List<int> aux2 = new List<int>();
+            SaidaPrimosDivisores saida = new SaidaPrimosDivisores();
+
+            saida.Entrada = numeroEntrada;
+
+            for (int i = numeroEntrada; i > 0; i--)
+            {
+                bool primo = true;
+                for (int j = i-1; j > 1; j--)
+                {
+                    if (i % j == 0)
+                    {
+                        primo = false;
+                    }
+                }
+                if (primo && numeroEntrada % i == 0)
+                {
+                    aux2.Add(i);
+                }
+                if (numeroEntrada % i == 0)
+                {
+                    aux.Add(i);
+                }
+            }
+            saida.Divisores = aux.OrderBy(x => x).ToArray();
+            saida.DivisoresPrimos = aux2.OrderBy(x => x).ToArray();
+
+            return Ok(saida);
+        }
+    }
+}
---
+++ API.PrimosDivisores/Estruturas/SaidaPrimosDivisores.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.PrimosDivisores.Estruturas
+{
+    public class SaidaPrimosDivisores
+    {
+        public int Entrada { set; get; }
+        public int[] Divisores { set; get; }
+        public int[] DivisoresPrimos { set; get; }
+    }
+}
